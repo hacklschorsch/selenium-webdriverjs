3c3ddf40c975785d629c71904f6e33082b47be78
Florian Sesser <fs@it-agenten.com>
2013-01-12 22:38:43 +0100

    Update from current upstream (r18456), version bump to 0.1.1
    Version bump just because it's been a while (3 months now),
    no code changes.

--- webdriver/lib/webdriver/http/http.js
+++ webdriver/lib/webdriver/http/http.js
@@ -243,6 +243,8 @@ webdriver.http.Executor.COMMAND_MAP_ = (function() {
           get('/session/:sessionId/element/:id/equals/:other')).
       put(webdriver.CommandName.SWITCH_TO_WINDOW,
           post('/session/:sessionId/window')).
+      put(webdriver.CommandName.MAXIMIZE_WINDOW,
+          post('/session/:sessionId/window/:windowHandle/maximize')).
       put(webdriver.CommandName.GET_WINDOW_POSITION,
           get('/session/:sessionId/window/:windowHandle/position')).
       put(webdriver.CommandName.SET_WINDOW_POSITION,
@@ -277,6 +279,14 @@ webdriver.http.Executor.COMMAND_MAP_ = (function() {
       put(webdriver.CommandName.MOVE_TO, post('/session/:sessionId/moveto')).
       put(webdriver.CommandName.SEND_KEYS_TO_ACTIVE_ELEMENT,
           post('/session/:sessionId/keys')).
+      put(webdriver.CommandName.ACCEPT_ALERT,
+          post('/session/:sessionId/accept_alert')).
+      put(webdriver.CommandName.DISMISS_ALERT,
+          post('/session/:sessionId/dismiss_alert')).
+      put(webdriver.CommandName.GET_ALERT_TEXT,
+          get('/session/:sessionId/alert_text')).
+      put(webdriver.CommandName.SET_ALERT_TEXT,
+          post('/session/:sessionId/alert_text')).
       build();
 
   /** @constructor */
